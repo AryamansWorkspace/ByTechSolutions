55065a3a6d174fd840041ba861a3a81bd61d6ad5
Aryaman Verma <amazingayv@gmail.com>
2026-03-05 00:24:29 +0530

    Add files via upload
    Fix project structure for Vercel deployment

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded",function(){
+
+const footer=document.querySelector(".footer");
+
+function revealFooter(){
+
+const footerTop=footer.getBoundingClientRect().top;
+const windowHeight=window.innerHeight;
+
+if(footerTop < windowHeight-100){
+footer.classList.add("visible");
+}
+
+}
+
+window.addEventListener("scroll",revealFooter);
+revealFooter();
+
+});
+
+function clearForm(e){
+
+e.preventDefault();
+document.getElementById("contactForm").reset();
+alert("Message Sent Successfully");
+
+}
